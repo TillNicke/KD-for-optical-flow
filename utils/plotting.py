0383e100da17f3291bb9c84e0e7872d4914d23ff
TNicke <33596932+TillNicke@users.noreply.github.com>
2021-05-18 18:35:18 +0200

    Add files via upload

---
+++ utils/plotting.py
@@ -0,0 +1,171 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors as mcolors
+import torch
+from utils.encoding import labelMatrixOneHot
+
+def showFlow(def_x):
+    x = def_x.squeeze().numpy()[0,:,:]
+    y = def_x.squeeze().numpy()[1,:,:]
+    #show flow map for numpy
+    H, W = x.shape
+    rho = np.sqrt(x*x+y*y)
+    theta = np.arctan2(x,-y)
+    theta2 = (-theta+np.pi)/(2.0*np.pi);
+    rho = np.clip(rho/np.percentile(rho, 99),0,1)
+    hsv = np.stack((theta2,rho,np.ones((H,W))),axis=2)
+    rgb = mcolors.hsv_to_rgb(hsv)
+    return rgb
+
+def overlaySegment(gray1,seg1,flag=False):
+    H, W = seg1.squeeze().size()
+    colors=torch.FloatTensor([0,0,0,199,67,66,225,140,154,78,129,170,45,170,170,240,110,38,111,163,91,235,175,86,202,255,52,162,0,183]).view(-1,3)/255.0
+    segs1 = labelMatrixOneHot(seg1.unsqueeze(0),8)
+
+    seg_color = torch.mm(segs1.view(8,-1).t(),colors[:8,:]).view(H,W,3)
+    alpha = torch.clamp(1.0 - 0.5*(seg1>0).float(),0,1.0)
+
+    overlay = (gray1*alpha).unsqueeze(2) + seg_color*(1.0-alpha).unsqueeze(2)
+    if(flag):
+        plt.imshow((overlay).numpy())
+        plt.show()
+    return overlay
+
+def countParameters(model):
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    params = sum([np.prod(p.size()) for p in model_parameters])
+    return params
+
+def flow2img(flow_data):
+	"""
+	convert optical flow into color image
+	:param flow_data:
+	:return: color image
+	"""
+	# print(flow_data.shape)
+	# print(type(flow_data))
+	u = flow_data[:, :, 0]
+	v = flow_data[:, :, 1]
+
+	UNKNOW_FLOW_THRESHOLD = 1e7
+	pr1 = abs(u) > UNKNOW_FLOW_THRESHOLD
+	pr2 = abs(v) > UNKNOW_FLOW_THRESHOLD
+	idx_unknown = (pr1 | pr2)
+	u[idx_unknown] = v[idx_unknown] = 0
+
+	# get max value in each direction
+	maxu = -999.
+	maxv = -999.
+	minu = 999.
+	minv = 999.
+	maxu = max(maxu, np.max(u))
+	maxv = max(maxv, np.max(v))
+	minu = min(minu, np.min(u))
+	minv = min(minv, np.min(v))
+
+	rad = np.sqrt(u ** 2 + v ** 2)
+	maxrad = max(-1, np.max(rad))
+	u = u / maxrad + np.finfo(float).eps
+	v = v / maxrad + np.finfo(float).eps
+
+	img = compute_color(u, v)
+
+	idx = np.repeat(idx_unknown[:, :, np.newaxis], 3, axis=2)
+	img[idx] = 0
+
+	return np.uint8(img)
+
+
+def compute_color(u, v):
+	"""
+	compute optical flow color map
+	:param u: horizontal optical flow
+	:param v: vertical optical flow
+	:return:
+	"""
+
+	height, width = u.shape
+	img = np.zeros((height, width, 3))
+
+	NAN_idx = np.isnan(u) | np.isnan(v)
+	u[NAN_idx] = v[NAN_idx] = 0
+
+	colorwheel = make_color_wheel()
+	ncols = np.size(colorwheel, 0)
+
+	rad = np.sqrt(u ** 2 + v ** 2)
+
+	a = np.arctan2(-v, -u) / np.pi
+
+	fk = (a + 1) / 2 * (ncols - 1) + 1
+
+	k0 = np.floor(fk).astype(int)
+
+	k1 = k0 + 1
+	k1[k1 == ncols + 1] = 1
+	f = fk - k0
+
+	for i in range(0, np.size(colorwheel, 1)):
+		tmp = colorwheel[:, i]
+		col0 = tmp[k0 - 1] / 255
+		col1 = tmp[k1 - 1] / 255
+		col = (1 - f) * col0 + f * col1
+
+		idx = rad <= 1
+		col[idx] = 1 - rad[idx] * (1 - col[idx])
+		notidx = np.logical_not(idx)
+
+		col[notidx] *= 0.75
+		img[:, :, i] = np.uint8(np.floor(255 * col * (1 - NAN_idx)))
+
+	return img
+
+
+def make_color_wheel():
+	"""
+	Generate color wheel according Middlebury color code
+	:return: Color wheel
+	"""
+	RY = 15
+	YG = 6
+	GC = 4
+	CB = 11
+	BM = 13
+	MR = 6
+
+	ncols = RY + YG + GC + CB + BM + MR
+
+	colorwheel = np.zeros([ncols, 3])
+
+	col = 0
+
+	# RY
+	colorwheel[0:RY, 0] = 255
+	colorwheel[0:RY, 1] = np.transpose(np.floor(255 * np.arange(0, RY) / RY))
+	col += RY
+
+	# YG
+	colorwheel[col:col + YG, 0] = 255 - np.transpose(np.floor(255 * np.arange(0, YG) / YG))
+	colorwheel[col:col + YG, 1] = 255
+	col += YG
+
+	# GC
+	colorwheel[col:col + GC, 1] = 255
+	colorwheel[col:col + GC, 2] = np.transpose(np.floor(255 * np.arange(0, GC) / GC))
+	col += GC
+
+	# CB
+	colorwheel[col:col + CB, 1] = 255 - np.transpose(np.floor(255 * np.arange(0, CB) / CB))
+	colorwheel[col:col + CB, 2] = 255
+	col += CB
+
+	# BM
+	colorwheel[col:col + BM, 2] = 255
+	colorwheel[col:col + BM, 0] = np.transpose(np.floor(255 * np.arange(0, BM) / BM))
+	col += + BM
+
+	# MR
+	colorwheel[col:col + MR, 2] = 255 - np.transpose(np.floor(255 * np.arange(0, MR) / MR))
+	colorwheel[col:col + MR, 0] = 255
+
+	return colorwheel
